1401ecef7a031da31159f120f3a165158d0baff8
TC Wang <wangtct@hotmail.com>
2024-05-14 16:55:53 +0800

    plan

--- pages/plan/training/index.vue
+++ pages/plan/training/index.vue
@@ -2,8 +2,223 @@
   <div class="container">
     <h1 class="px-2">Training Material - Data Science</h1>
     <div class="flex flex-row flex-wrap">
-      <div class="w-full md:w-1/2 p-2 shadow-md"></div>
-      <div class="w-full md:w-1/2 p-2 shadow-md"></div>
+      <div class="w-full md:w-1/2 p-2 shadow-md">
+        <h3>Reference Books</h3>
+        <ul>
+          <li>
+            <a
+              href="https://www.amazon.com/-/zh_TW/Lillian-Pierson/dp/1119811554/ref=sr_1_7?crid=3LJ5HTKCMC3L9&dib=eyJ2IjoiMSJ9.PziwHT_CjbyPzE1bnR2s6xVc-TvJppVNn2ezrW6T7156WsCH2shKoI50hjv5di3EAegwIBIo6znZk_cZT9Q9NEfPlm5TPrbMEnIBOwXw2xMDbtUHd6d5N94m-oP3CK09sr7ItU1rZIxQ1kaKiAwIkk2W30p2KmlSi8VabApQfkZUo1MtB5KLgfx5kcPm99ojHiGLX_OOqST0OUZts3ONleBn-H_xmRtQI92btfpO8NK-LjR1xL9TOd5hCUbUgeIScFxNccXo8AE8F2EADbkUNplKUa3ovu5deUKXQKWhdsA.wS0045zwnMzgD6lEuOcNT1GNorrDq5JqXCEn-vHBmi4&dib_tag=se&keywords=data+science&qid=1715674141&sprefix=data+science%2Caps%2C249&sr=8-7"
+              >Data Science For Dummies</a
+            >
+          </li>
+          <li>
+            <a href="/files/DSML-001-Data Science and Machine Learning.pdf"
+              >DSML-001-Data Science and Machine Learning.pdf</a
+            >
+          </li>
+          <li>
+            <a
+              href="/files/DSML-003-Data-Science-Concepts-and-Practice-2nd-Edition-3.pdf"
+              >DSML-003-Data-Science-Concepts-and-Practice-2nd-Edition-3.pdf</a
+            >
+          </li>
+          <li>
+            <a href="/files/DSML-006-The Evolution of Data Science.pdf"
+              >DSML-006-The Evolution of Data Science.pdf</a
+            >
+            - Data Science for Beginners : Comprehensive Guide to Most Important
+            Basic in Data Science
+          </li>
+          <li>
+            <a href="/files/DSML-007-Principles of Data Science.pdf"
+              >DSML-007-Principles of Data Science.pdf</a
+            >
+          </li>
+        </ul>
+
+        <h3>Video Training - Coursera</h3>
+        <ul>
+          <li>
+            Essential
+            <ul>
+              <li>
+                <a href="https://bit.ly/3Rn00ZA"
+                  >IBM Data Science Professional Certification</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3d1QATT"
+                  >Machine Learning Specialization</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3KPfint"
+                  >Deep Learning Specialization</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3TKnoBS"
+                  >Practical Data Science on AWS</a
+                >
+              </li>
+              <li>
+                <a href="https://imp.i384100.net/QO3N7M"
+                  >Data Science and Machine Learning on Google Cloud</a
+                >
+              </li>
+            </ul>
+          </li>
+          <li>
+            Very Good
+            <ul>
+              <li>
+                <a href="https://bit.ly/3TOeh38"
+                  >IBM Data Analyst Professional Certification</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3RhvfFp"
+                  >John Hopkins University Data Science Specialization</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3D04S2d"
+                  >University of Washington Machine Learning Specialization</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3ASUtTi">ICL Math for ML</a>
+              </li>
+              <li>
+                <a href="https://bit.ly/3AWRrxE">DeepLearning.AI MLOps</a>
+              </li>
+              <li>
+                <a href="https://bit.ly/3cPikLQ"
+                  >Google Data Analytics Professional Certification</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3cMwqO3">IBM Machine Learning</a>
+              </li>
+              <li>
+                <a href="https://bit.ly/3RoYpCn"
+                  >University of Pennsylvania Business Analytics</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3BePQV2"
+                  >DeepLearning.AI TensorFlow Developer</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3wUHorl"
+                  >University of Alberta Reinforcement Learning</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3RFj4SA"
+                  >DeepLearning.AI AI for Everyone</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3q966AN"
+                  >Data Visualization with Tableau</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3RlkFNk"
+                  >UMichigan Statistics with Python / Applied Data Science</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3QkicBJ"
+                  >John Hopkins Advanced Statistics</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3QkEgvM">Stanford Statistics</a>
+              </li>
+              <li>
+                <a href="https://bit.ly/3BgKBEo"
+                  >TensorFlow Advanced Techniques</a
+                >
+              </li>
+              <li>
+                <a href="https://bit.ly/3erVdqT">ohn Hopkins Algebra</a>
+              </li>
+              <li>
+                <a href="https://bit.ly/3BghQaN"
+                  >Learn SQL Basics U California Davis</a
+                >
+              </li>
+            </ul>
+          </li>
+          <li>
+            Aovid
+            <ul>
+              <li></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/2 p-2 shadow-md">
+        <h2>Training Course</h2>
+        <ul>
+          <li>
+            coursera
+            <a
+              href="https://www.coursera.org/professional-certificates/ibm-data-science?utm_source=gg&utm_medium=sem&utm_campaign=b2c_apac_ibm-data-science_ibm_ftcof_professional-certificates_arte_dec-23_dr_geo-set-1-multi_pmax_gads_lg-all&utm_content=b2c&campaignid=20896738298&adgroupid=&device=c&keyword=&matchtype=&network=x&devicemodel=&adpostion=&creativeid=&hide_mobile_promo&gad_source=1&gclid=EAIaIQobChMIl4m-t9SMhgMVCMJMAh1AwQX9EAAYASAAEgKB8vD_BwE"
+              >IBM Data Science
+            </a>
+            - 5 months at 10 hours a week
+          </li>
+          <li>
+            MIT
+            <a
+              href="https://www.mygreatlearning.com/mit-idss-data-science-machine-learning-program?&utm_source=Google&utm_medium=search&utm_campaign=DSML_Int_Search_consolidated_Phrase_SEA&adgroup_id=159319537872&campaign_id=21015060403&keyword=data%20analysis%20certificate&ad_id=690519805886&placement=&gad_source=1&gclid=EAIaIQobChMIl4m-t9SMhgMVCMJMAh1AwQX9EAAYAiAAEgJIiPD_BwE"
+              >Data Science &amp; Machine Learning Program
+            </a>
+            by MIT IDSS, 12 Weeks Program
+          </li>
+          <li>
+            Linkedin Learning
+            <a
+              href="https://www.linkedin.com/learning/paths/become-a-data-scientist"
+              >Become a Data Scientist</a
+            >
+            12 courses
+          </li>
+        </ul>
+        <h3>Study Guide</h3>
+        <ul>
+          <li>
+            simplilean
+            <a
+              herf="https://www.simplilearn.com/tutorials/data-science-tutorial/how-to-become-a-data-scientist"
+              >How to Become a Data Scientist in 2024: Complete Guide</a
+            >
+          </li>
+          <li>
+            geeksforgeeks
+            <a href="https://www.geeksforgeeks.org/data-scientist-roadmap/"
+              >Data Scientist Roadmap – A Complete Guide</a
+            >
+          </li>
+          <li>
+            edx
+            <a href="https://www.edx.org/become/how-to-become-a-data-scientist"
+              >How to become a data scientist in 6 steps</a
+            >
+          </li>
+          <li>
+            learn
+            <a
+              href="https://www.datacamp.com/blog/how-to-become-a-data-scientist"
+              >How to Become a Data Scientist in 8 Steps</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
--- pages/survey/market/index.vue
+++ pages/survey/market/index.vue
@@ -2,7 +2,52 @@
   <div class="container">
     <h1 class="px-2">Market Survey - Data Science</h1>
     <div class="flex flex-row flex-wrap">
-      <div class="w-full md:w-1/2 p-2 shadow-md"></div>
+      <div class="w-full md:w-1/2 p-2 shadow-md">
+        <h2>Job Search</h2>
+        <ul>
+          <li>
+            Post
+            <a href="https://www.datasciencejobscanada.com/"
+              >Data Science Jobs Canada</a
+            >
+          </li>
+          <li>
+            indeed
+            <a
+              href="https://ca.indeed.com/jobs?q=Data+Science&l=Montreal%2C+QC&from=searchOnDesktopSerp&vjk=26d3270d3ec76d5b"
+              >Data Science in Montreal, QC</a
+            >
+          </li>
+          <li>
+            Linkedin
+            <a
+              href="https://ca.linkedin.com/jobs/search?keywords=Data%20Scientist&location=Montreal%2C%20Quebec%2C%20Canada&geoId=101330853&trk=public_jobs_jobs-search-bar_search-submit&original_referer=https%3A%2F%2Fca.linkedin.com%2Fjobs%2Fdata-scientist-jobs%3Fposition%3D1%26pageNum%3D0&position=1&pageNum=0"
+              >Data Scientist in Montreal, QC</a
+            >
+          </li>
+          <li>
+            Glassdoor
+            <a
+              href="https://www.glassdoor.ca/Job/montreal-qc-canada-data-science-jobs-SRCH_IL.0,18_IC2296722_KO19,31.htm"
+              >data science in Montreal, QC</a
+            >
+          </li>
+          <li>
+            workopolis
+            <a
+              href="https://www.workopolis.com/search?q=data+scientist&l=Montreal%2C+QC"
+              >data scientist in Montreal, QC</a
+            >
+          </li>
+          <li>
+            ZipRecruiter
+            <a
+              href="https://www.ziprecruiter.com/jobs-search?search=Data+Scientist&location=Montreal%2C+QC&radius=25&lvk=nBtDVh9LBs1r-s_Z8qqSgA.--NNNb3Do9V"
+              >data scientist in Montreal, QC</a
+            >
+          </li>
+        </ul>
+      </div>
       <div class="w-full md:w-1/2 p-2 shadow-md"></div>
     </div>
   </div>
